99331f82539d41b1baa1c1038505fa5254c83953
declan-taylor <declan.taylor@alumni.ubc.ca>
2023-02-10 01:19:01 -0800

    Added line breaks to ch17 and re-ran scripts.

--- scripts/compileDOIs.R
+++ scripts/compileDOIs.R
@@ -1,16 +1,44 @@
+# Load libraries
+library(tidyverse)
+library(bibtex)
+library(here)
+library(revtools)
+
+# Set reference directory
+setwd(here("data/AR6_bibFiles"))
+Files <- list.files(pattern = ".bib")
+
+# A function to extract DOIs and relevant information from each chapter. Every
+# DOI is contained on one line with accompanying ID information.
 extractDOIs <- function(files){
-  DOIs <- tibble(WG = character(), 
+  # Create blank tibble to be appended to in the for loop.
+  DOIs <- tibble(WG = character(),
                  chapter = character(),
-                 numCitations = numeric(),
-                 numDOI = numeric())
+                 label = character(),
+                 doi = character(),
+                 url = character(),
+                 isbn = character(),
+                 issn = character())
+  
   for(i in files){
     bibDF <- read_bibliography(i) %>%
+      # Extract the name of the working group and chapter to be added as 
+      # columns to support the long-data format.
       mutate(WG = str_extract(i, "WG[I]{1,3}(?=_)"),
              chapter = str_extract(i, "(?<=References_)[A-z0-9]*(?=\\.)")) %>%
       select(any_of(c("WG", "chapter", "label", "doi", "url", "isbn", "issn")))
-    # Write an appropriatley named CSV for each chapter that
-    write_csv(bibDF, file = paste0(str_extract(i, ".+(?=.bib)"),"_DOIs.csv"))
+    #Save the dataframe to the local function environment
+    # assign(paste0(str_extract(i, ".+(?=.bib)")), bibDF)
+    
+    # Append the chapter's data to the overall DOI tibble in the function 
+    # environment.
+    DOIs <- bind_rows(DOIs, bibDF)
+    # ...And update in the global environemnt!
+    DOIs <<- DOIs
   } 
 }
 
+# Run the above function
 extractDOIs(Files)
+# Save the tibble output as a CSV
+write_csv(DOIs, file = here("data/AR6_DOIs.csv"))
