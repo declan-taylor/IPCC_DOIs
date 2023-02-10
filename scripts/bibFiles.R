b813486059097544f2aaac1d26993a1119fa92aa
declan-taylor <declan.taylor@alumni.ubc.ca>
2023-02-10 08:36:17 -0800

    Add analysis of URL coverage in addition to DOIs.

--- scripts/bibFiles.R
+++ scripts/bibFiles.R
@@ -15,10 +15,7 @@ Files <- list.files(pattern = ".bib")
 # access from the IPCC's .bib files.
 explainCitations <- function(files){
   # Define an empty tibble to be filled.
-  citationInfo <- tibble(WG = character(), 
-                            chapter = character(),
-                            numCitations = numeric(),
-                            numDOI = numeric())
+  citationInfo <- tibble()
   
   for(i in files){
     bibDF <- read_bibliography(i)
@@ -27,10 +24,18 @@ explainCitations <- function(files){
     WG <- str_extract(i, "WG[I]{1,3}(?=_)")
     chapter <- str_extract(i, "(?<=References_)[A-z0-9]*(?=\\.)")
     numCitations <- nrow(bibDF)
-    numDOI <- length(unique(bibDF$doi))
+    # Sometimes the numDOI count is 1 higher than it should be as NA counts as 
+    # a unique entry. I consider this acceptable error.
+    numDOI <- nrow(filter(bibDF, !is.na(doi)))
+    # A count of references that do not have DOIs but do have some other sort of
+    # identifier.
+    numURL <- nrow(filter(bibDF, is.na(doi) & !is.na(url)))
+    # Rounded percentage of number of references with DOIs
     percentDOI = round(100*(1-((numCitations - numDOI)/numCitations)))
+    # Total coverage if we include url, issn, isbn.
+    percentURL = round(100*(1-((numCitations - (numDOI + numURL))/numCitations)))
     # Append the extracted data to a dataframe for easy review.
-    citationInfo <- bind_rows(citationInfo, tibble(WG, chapter, numCitations, numDOI, percentDOI))
+    citationInfo <- bind_rows(citationInfo, tibble(WG, chapter, numCitations, numDOI, numURL, percentDOI, percentURL))
     citationInfo <<- citationInfo
   } 
 }
